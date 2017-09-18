75f5bca2cdb77859b864a177a940858c48c1c92f
Frelin Maxime <frelinmaxime@gmail.com>
2017-09-18 18:58:02 +0200

    Projet initial vierge

---
+++ app/src/main/java/ca/qc/cqmatane/informatique/cataloguefauneetflore/VueFauneEtFloreAfficher.java
@@ -0,0 +1,13 @@
+package ca.qc.cqmatane.informatique.cataloguefauneetflore;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class VueFauneEtFloreAfficher extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.faune_et_flore_afficher);
+    }
+}
