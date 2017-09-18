dec26d0ff7e13c50265aeb87fa7b1bb5df346b02
Frelin Maxime <frelinmaxime@gmail.com>
2017-09-18 19:40:18 +0200

    Ajout du début de base de donnée locale (pour les tests)
    Il manque les tables qui n'ont pas été encore crées

--- app/src/main/java/ca/qc/cqmatane/informatique/cataloguefauneetflore/donnees/BaseDeDonnee.java
+++ app/src/main/java/ca/qc/cqmatane/informatique/cataloguefauneetflore/donnees/BaseDeDonnee.java
@@ -1,9 +1,46 @@
 package ca.qc.cqmatane.informatique.cataloguefauneetflore.donnees;
 
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
 /**
  * Created by Maxime on 18/09/2017.
  */
 
-public class BaseDeDonnee {
+public class BaseDeDonnee extends SQLiteOpenHelper {
 
-}
+        private static BaseDeDonnee instance = null;
+
+        public static BaseDeDonnee getInstance(Context contexte) {
+            if (null == instance) instance = new BaseDeDonnee(contexte);
+            return instance;
+        }
+
+        public static BaseDeDonnee getInstance() {
+            return instance;
+        }
+
+        public BaseDeDonnee(Context contexte) {
+            super(contexte, "catalogue_faune_et_flore", null, 1);
+        }
+
+        @Override
+        public void onCreate(SQLiteDatabase db) {
+            String CREATE_TABLE = "";
+            db.execSQL(CREATE_TABLE);
+        }
+
+        @Override
+        public void onUpgrade(SQLiteDatabase db, int arg1, int arg2) {
+            String CREATE_TABLE = "";
+            db.execSQL(CREATE_TABLE);        }
+
+        @Override
+        public void onOpen(SQLiteDatabase db) {
+            //db.execSQL("DROP TABLE rendez_vous");
+            String CREATE_TABLE = "";
+            db.execSQL(CREATE_TABLE);
+        }
+
+    }
