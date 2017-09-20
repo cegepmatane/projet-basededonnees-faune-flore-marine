7620e316c9e5f0c640f27c3280ce988327eb8bbe
Frelin Maxime <frelinmaxime@gmail.com>
2017-09-20 20:46:34 +0200

    Creation du modele Faune et fin des modeles

--- app/src/main/java/ca/qc/cqmatane/informatique/cataloguefauneetflore/modele/Faune.java
+++ app/src/main/java/ca/qc/cqmatane/informatique/cataloguefauneetflore/modele/Faune.java
@@ -4,6 +4,14 @@ package ca.qc.cqmatane.informatique.cataloguefauneetflore.modele;
  * Created by Maxime on 20/09/2017.
  */
 
-public class Faune {
-    
+public class Faune extends Espece {
+
+    private String type;
+    private int population;
+
+    public Faune(String nom, String nomScientifique, String lieu, String type, int population) {
+        super(nom, nomScientifique, lieu);
+        this.type = type;
+        this.population = population;
+    }
 }
---
+++ app/src/main/java/ca/qc/cqmatane/informatique/cataloguefauneetflore/modele/Flore.java
@@ -0,0 +1,12 @@
+package ca.qc.cqmatane.informatique.cataloguefauneetflore.modele;
+
+/**
+ * Created by Maxime on 20/09/2017.
+ */
+
+public class Flore extends  Espece{
+    
+    public Flore(String nom, String nomScientifique, String lieu) {
+        super(nom, nomScientifique, lieu);
+    }
+}
