46238d8bcac87b9bd40257311640aad397ff9f1c
GiovanniScarpellino <scarpellino.giovanni@gmail.com>
2017-09-20 14:58:59 -0400

    Suppression de FauneEtFloreDAO.java ou créer FauneDAO.java et FloreDAO.java

--- app/src/main/java/ca/qc/cqmatane/informatique/cataloguefauneetflore/donnees/FauneDAO.java
+++ app/src/main/java/ca/qc/cqmatane/informatique/cataloguefauneetflore/donnees/FauneDAO.java
@@ -1,8 +1,32 @@
 package ca.qc.cqmatane.informatique.cataloguefauneetflore.donnees;
 
+import ca.qc.cqmatane.informatique.cataloguefauneetflore.modele.Faune;
+
 /**
  * Created by Maxime on 20/09/2017.
  */
 
 public class FauneDAO {
+
+    private static FauneDAO instance;
+    private BaseDeDonnee accesseurBaseDeDonnee;
+
+    public static FauneDAO getInstance() {
+        if(instance == null)instance = new FauneDAO();
+        return instance;
+    }
+
+    private FauneDAO(){
+        accesseurBaseDeDonnee = BaseDeDonnee.getInstance();
+    }
+
+    public void modifierFaune(Faune faune) {
+        String SQL_UPDATE = "UPDATE faune set " +
+                "nom=" + faune.getNom() + ", " +
+                "nomScientifique=" + faune.getNomScientifique() + ", " +
+                "lieu=" + faune.getLieu() + ", " +
+                "type=" + faune.getType() + ", " +
+                "population=" + faune.getPopulation();
+        accesseurBaseDeDonnee.getWritableDatabase().execSQL(SQL_UPDATE);
+    }
 }
--- app/src/main/java/ca/qc/cqmatane/informatique/cataloguefauneetflore/donnees/FloreDAO.java
+++ app/src/main/java/ca/qc/cqmatane/informatique/cataloguefauneetflore/donnees/FloreDAO.java
@@ -1,8 +1,31 @@
 package ca.qc.cqmatane.informatique.cataloguefauneetflore.donnees;
 
+import ca.qc.cqmatane.informatique.cataloguefauneetflore.modele.Flore;
+
 /**
  * Created by Maxime on 20/09/2017.
  */
 
 public class FloreDAO {
+
+    private static FloreDAO instance;
+    private BaseDeDonnee accesseurBaseDeDonnee;
+
+    public static FloreDAO getInstance() {
+        if(instance == null)instance = new FloreDAO();
+        return instance;
+    }
+
+    private FloreDAO() {
+        accesseurBaseDeDonnee = BaseDeDonnee.getInstance();
+    }
+
+    public void modifierFlore(Flore flore) {
+        String SQL_UPDATE = "UPDATE flore set " +
+                "nom=" + flore.getNom() + ", " +
+                "nomScientifique=" + flore.getNomScientifique() + ", " +
+                "lieu=" + flore.getLieu() + ", ";
+        accesseurBaseDeDonnee.getWritableDatabase().execSQL(SQL_UPDATE);
+    }
+
 }
