e4d7bad405845bb934d23788f47c2c7dff7fef22
GiovanniScarpellino <scarpellino.giovanni@gmail.com>
2017-09-20 15:03:53 -0400

    Ajout de l'activité de modification de faune et flore

---
+++ app/src/main/java/ca/qc/cqmatane/informatique/cataloguefauneetflore/VueFauneEtFloreModifier.java
@@ -0,0 +1,13 @@
+package ca.qc.cqmatane.informatique.cataloguefauneetflore;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class VueFauneEtFloreModifier extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.vue_faune_et_flore_modifier);
+    }
+}
