53a3ee3165efe037829abd97c88eb9ea6794d6e4
GiovanniScarpellino <scarpellino.giovanni@gmail.com>
2017-09-24 19:03:58 -0400

    Ajout des vue Faune et Flore modifier

--- app/src/main/java/ca/qc/cqmatane/informatique/cataloguefauneetflore/VueFloreModifier.java
+++ app/src/main/java/ca/qc/cqmatane/informatique/cataloguefauneetflore/VueFloreModifier.java
@@ -3,11 +3,11 @@ package ca.qc.cqmatane.informatique.cataloguefauneetflore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class VueFauneEtFloreModifier extends AppCompatActivity {
+public class VueFloreModifier extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.vue_faune_et_flore_modifier);
+        setContentView(R.layout.vue_flore_modifier);
     }
 }
