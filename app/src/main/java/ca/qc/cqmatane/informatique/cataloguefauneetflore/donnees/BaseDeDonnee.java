7564abbd6f0c4f367751fccc9877622a25ad830f
ppenelon <penelon.pierre@gmail.com>
2017-11-15 14:23:37 -0500

    Suppression de commentaires inutiles dans la base de données locale

--- app/src/main/java/ca/qc/cqmatane/informatique/cataloguefauneetflore/donnees/BaseDeDonnee.java
+++ app/src/main/java/ca/qc/cqmatane/informatique/cataloguefauneetflore/donnees/BaseDeDonnee.java
@@ -4,10 +4,6 @@ import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
-/**
- * Created by Maxime on 18/09/2017.
- */
-
 public class BaseDeDonnee extends SQLiteOpenHelper {
 
     private static BaseDeDonnee instance = null;
@@ -43,18 +39,10 @@ public class BaseDeDonnee extends SQLiteOpenHelper {
 
     @Override
     public void onOpen(SQLiteDatabase db) {
-//        db.execSQL("DROP TABLE faune");
-//        db.execSQL("DROP TABLE flore");
         String CREATE_TABLE = "CREATE TABLE IF NOT EXISTS faune(idFaune INTEGER PRIMARY KEY, nom TEXT, nomScientifique TEXT, lieu TEXT, type TEXT, population TEXT)";
         db.execSQL(CREATE_TABLE);
         CREATE_TABLE = "CREATE TABLE IF NOT EXISTS flore(idFlore INTEGER PRIMARY KEY, nom TEXT, nomScientifique TEXT, lieu TEXT)";
         db.execSQL(CREATE_TABLE);
-
-        //db.execSQL("INSERT INTO faune VALUES (null, 'Poisson', 'Poisson', 'Fleuve Saint Laurent', 'Poisson', 1552)");
-
-        //db.execSQL("INSERT INTO flore VALUES (null, 'Algue', 'Algue Marine', 'Fleuve Saint Laurent')");
-
-
     }
 
 }
